ea3d36485b44a4bf691f10e51226310e2affa6ef
ahmed <ahmed.elghoul@infor.com>
2022-04-17 16:53:16 +0100

    test version

--- enterprise-ng-quickstart/src/app/month/month.component.ts
+++ enterprise-ng-quickstart/src/app/month/month.component.ts
@@ -4,44 +4,109 @@ import {
   OnInit,
   ViewChild
 } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
 // @ts-ignore
 import { SohoBarComponent, SohoCalendarComponent, SohoToastService } from 'ids-enterprise-ng';
 
-
+export interface Calendar {
+  name: string;
+  dates?: any;
+}
 @Component({
   selector: 'app-month',
   templateUrl: 'month.component.html',
 })
-export class MonthComponent   {
+export class MonthComponent extends ComponentStore<Calendar>   {
 
+
+  name$ = this.select(state => state.name);
+  dates$ = this.select(state => state.dates);
+
+  
   @HostBinding('style.overflow') overflow = 'auto';
   @HostBinding('style.height') height = 'auto';
   @HostBinding('style.display') block = 'block';
 
   @ViewChild(SohoCalendarComponent) sohoCalendarComponent?: SohoCalendarComponent;
 
-  public initialMonth = 1;
-  public initialYear = 2019;
+  
   public showViewChanger = true;
-  public eventTypes?: [];
-  public events?: [];
+  eventTypes= [  {
+    "id": "default",
+    "label": "Discretionary Time Off",
+    "translationKey": "DiscretionaryTimeOff",
+    "color": "amber",
+    "checked": true,
+    "click": null
+  },
+  {
+    "id": "summerWE",
+    "label": "Admin",
+    "translationKey": "AdministrativeLeave",
+    "color": "green",
+    "checked": true,
+    "click": null
+  },];
+ 
   public iconTooltip = 'status';
   public eventTooltip = 'comments';
- 
+  public eventsLoaded = false;
 
   public onCalendarDateSelectedCallback = (_node: Node, args: SohoCalendarDateSelectedEvent) => {
     console.log('onCalendarEventSelectedCallback', args);
   }
 
-  constructor( private toastService: SohoToastService) { }
+  constructor( private toastService: SohoToastService) {
+
+    super({ name: 'customer-calendar', dates: []});
+ 
+   }
+ 
+
+ 
 
   onRenderMonth(event: SohoCalendarRenderMonthEvent) {
     console.log('onRenderMonth', event);
+    if (this.eventsLoaded) {
+      return;
+    }
+ 
+    this.setState((state) => { return { ...state, dates: [
+      {
+        "id": "1",
+        "subject": "Default",
+        "shortSubject": "DTO",
+        "comments": "Short getaway",
+        "location": "Us Office",
+        "status": "Draft",
+        "starts": "2022-03-01T10:00:00.999",
+        "ends": "2022-03-20T11:00:00.999",
+        "type": "default",
+        "isAllDay": "true"
+      },
+      {
+        "id": "2",
+        "subject": "Summer WE",
+        "comments": "Quick check-up",
+        "location": "Us Office",
+        "status": "Draft",
+        "starts": "2022-03-21T14:00:00.999",
+        "ends": "2022-03-21T18:00:00.999",
+        "type": "default",
+        "isAllDay": "false"
+      }
+    ] } })
+        this.eventsLoaded = true;
+    
   }
+
+
 
   onSelected(event: SohoCalendarDateSelectedEvent) {
     console.log('onSelected', event);
   }
+
+ 
 
   onEventClicked(event: SohoCalendarEventClickEvent) {
     this.toastService.show({ title: 'Calendar Test', message: 'Event "' + event?.event?.subject + '" Clicked' });
